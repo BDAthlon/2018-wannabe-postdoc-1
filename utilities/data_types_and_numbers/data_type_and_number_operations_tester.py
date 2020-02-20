ad8d4dae5f43b073de4e7c84d26d8ee0828e9f02
Zhiyang Giovanni Ong <zhiyang@client-10-229-227-195.tamulink.tamu.edu>
2020-02-20 15:58:10 -0600

    Update build via Python script

--- utilities/data_types_and_numbers/data_type_and_number_operations_tester.py
+++ utilities/data_types_and_numbers/data_type_and_number_operations_tester.py
@@ -287,7 +287,7 @@ class data_type_n_number_ops_tester:
 			statistical_analysis.increment_number_test_cases_passed()
 		else:
 			print(prompt .format("FAIL!!!"))
-		prompt = "	... Test: '010011' == [0,1,0,0,1,1]		{}"
+		prompt = "	... Test: '010011' == [0,1,0,0,1,1]			{}"
 		statistical_analysis.increment_number_test_cases_used()
 		if [0,1,0,0,1,1] == data_type_n_number_ops.convert_binary_string_to_list_of_0_1_integers("010011"):
 			print(prompt .format("OK"))
